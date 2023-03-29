85664426ce54b21e81f2949b240f9cf8aa5779f0
Nazarov Dmitry <Difat1@inbox.ru>
2023-03-29 18:44:44 +0300

    Rename shema

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 export type UserDocument = User & Document;
 import * as mongoose from 'mongoose';
+
 @Schema()
 export class User {
   @Prop({ required: true, unique: true })
